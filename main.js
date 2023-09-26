c182fe2e375a9f9bcacce05e01d4b9f52d463322
WebSyncMedia <145934135+WebSyncMedia@users.noreply.github.com>
2023-09-26 22:21:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,22 @@ $(document).ready(function(){
     $(".svg").fadeIn(20000);
 });
 
+function myFunction(x) {
+    x.classList.toggle("change");
+    document.getElementById("Pb-container").style.dislpay = "block";
+
+    var acc = document.getElementByClassName("container");
+    var i;
+
+    for(i = 0; i < acc.length; i++) {
+        acc[i].addEventListener.("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+}
